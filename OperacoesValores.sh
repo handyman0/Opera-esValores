7d0cacb7050e820b8d652777dac0c8292aa7dca8
handyman0 <luiz.number@gmail.com>
2023-11-20 15:06:15 -0300

    corrigindo alguns erros não vistos anteriormente

--- OperacoesValores.sh
+++ OperacoesValores.sh
@@ -15,31 +15,35 @@ case "$opcao" in
 	1)
 		echo "Você escolheu soma!"
 		echo ""
-		RESULTADO=$(expr $VALOR1 + $VALOR2)
-		echo "$expr $VALOR1 + $VALOR2 = $RESULTADO"
+		RESULTADO=$((VALOR1 + VALOR2))
+		echo "$VALOR1 + $VALOR2 = $RESULTADO"
 		sleep 3
 		;;
 	2)
-                echo "Você escolheu subtração!"
-		expr $VALOR1 - $VALOR2
-		sleep 3
-                ;;
-	3)
-                echo "Você escolheu Multiplicação!"
-		expr $VALOR1 * $VALOR2
-		sleep 3
-                ;;
-	4)
-                echo "Você escolheu Divisão!"
-                expr $VALOR1 / $VALOR2
+		echo "Você escolheu subtração!"
+		RESULTADO=$((VALOR1 - VALOR2))
+		echo "$VALOR1 - $VALOR2 = $RESULTADO"
 		sleep 3
 		;;
-	Q|q) 
-                echo "Saindo ..."
+	3)
+		echo "Você escolheu Multiplicação!"
+		RESULTADO=$((VALOR1 * VALOR2))
+		echo "$VALOR1 * $VALOR2 = $RESULTADO"
+		sleep 3
+		;;
+	4)
+		echo "Você escolheu Divisão!"
+		RESULTADO=$((VALOR1 / VALOR2))
+		echo "$VALOR1 / $VALOR2 = $RESULTADO"
+		sleep 3
+		;;
+	[Qq])
+		echo "Saindo ..."
 		exit 0
-                ;;
+		;;
 	*)
-                echo "Escolha uma opção valida"
+		echo "Escolha uma opção válida"
 		exit 1
-                ;;
+		;;
 esac
+
